de63752bae6d3dea660c7f428f6bbcf2c498a984
omar201014 <omar_hussein201014@hotmail.com>
2022-03-02 19:46:22 +0200

    site-updated

--- js/script.js
+++ js/script.js
@@ -6,5 +6,31 @@ $(document).ready(function(){
         margin:10,
         nav:true,
         dots:false,
+        responsive : {
+            // breakpoint from 0 up
+            0 : {
+                loop:true,
+                items:1,
+                margin:10,
+                nav:true,
+                dots:false
+                    },
+            // breakpoint from 480 up
+            480 : {
+                loop:true,
+                items:1,
+                margin:10,
+                nav:true,
+                dots:false
+            },
+            // breakpoint from 768 up
+            768 : {
+                loop:true,
+                items:2,
+                margin:10,
+                nav:true,
+                dots:false
+            }
+        }
     });        
   });
